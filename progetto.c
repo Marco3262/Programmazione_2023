f9a0d986a2485cb7a458150bd3e6a17991683c47
Marco3262 <marcospinetta@libero.it>
2022-12-17 21:46:36 +0100

    Update progetto.c

--- progetto.c
+++ progetto.c
@@ -56,7 +56,8 @@ int main()
     val = validator_cycle();
     
     /* Si avvia l'opzione inserita */
-    switch(val){
+    switch(val)
+    {
         case 0:
             beal_conjecture();
             break;
@@ -79,22 +80,25 @@ int validator_cycle()
         control;        /* Lavoro: variabile di controllo della scanf */
 
     /* Acquisisco valore */ 
-    control = scanf("%d",&val);
+    control = scanf("%d",
+                    &val);
     
     /* Avvio un ciclo quando il valore non è compreso tra 0 e 2 */
-    while(val < 0 || val > 2 || control != 1){
+    while(val < 0 ||
+          val > 2 ||
+          control != 1)
+    {
             
             printf("Il valore non è corretto. Riprova: ");
             /* Pulisco il buffer */
             scanf("%*[^\n]");
             /* Riacquisisco il valore */
-            control = scanf("%d",&val);
+            control = scanf("%d",
+                            &val);
         
     }
-    
     /* Restituisco il valore validato */
     return val;
-
 }
 
 /* Funzione per la validazione di un intero maggiore di n*/
@@ -106,52 +110,58 @@ int validator_num(int n,            /* Lavoro: variabile che identifica il valor
         control;        /* Lavoro: variabile di controllo della scanf */
     
     /* Acquisisco valore */ 
-    control = scanf("%d",&val);
+    control = scanf("%d",
+                    &val);
     /* Avvio un ciclo che impone che il valore sia maggiore di n */
-    while(val < n || control != 1){
-            
+    while(val < n ||
+          control != 1)
+    {
+
             /* Faccio un controllo per la stampa */
-            if(x == 'y'){
+            if(x == 'y')
+            {
                 printf("Deve essere un NUMERO MAGGIORE di %d.\n"
-                        "Inserisci di nuovo: ",n-1);
-            }else{
+                        "Inserisci di nuovo: ",
+                        n-1);
+            }else
+            {
                 printf("Deve essere un NUMERO MAGGIORE UGUALE di %d.\n"
-                    "Inserisci di nuovo: ",n);
+                       "Inserisci di nuovo: ",
+                       n);
             }
             
             /* Pulisco il buffer */
             scanf("%*[^\n]");
             /* Riacquisisco il valore */
-            control = scanf("%d",&val);
-        
+            control = scanf("%d",
+                            &val);   
     }
-    
     /* Restituisco il valore validato */
     return val;
-
 }
 
 /* Funzione che stabilisce se il numero inserito è un numero primo */
 int validator_first_num(int val)    /* Input: variabile che identifica il valore da validare */
 {
-    
     int i;              /* Lavoro: variabile contatore di ciclo */
     int control = 1;    /* Lavoro: variabile di controllo che indica se il numero è primo */
         
     /*  Verifico che il numero inserito sia un numero primo
         dividendolo ogni volta per due in su e verificando
         se si ottiene un resto oppure no */
-    for(i = 2; control == 1 && i < val; i++){
-        
-        if(val%i == 0){
+    for(i = 2;
+        control == 1 &&
+        i < val;
+        i++)
+    {
+        if(val%i == 0)
+        {
             /*  Se non c'è resto vuol dire che il numero 
                 non è primo poichè è divisibile perciò 
                 setto control a zero e richiedo un nuovo inserimento */
-            control = 0;
-            
+            control = 0; 
         }
     }
-    
     /* Restituisco il risultato */
     return control;
 }
@@ -161,14 +171,15 @@ void common_first_num(int a,        /* Input: variabile che identifica il valore
                       int b,        /* Input: variabile che identifica il valore da confrontare */
                       int c)        /* Input: variabile che identifica il valore da confrontare */
 {
-    
     int i,              /* Lavoro: variabile contatore di ciclo */
         control = 0,    /* Lavoro: variabile di controllo che identifica se un numero è primo */
         count = 0;      /* Lavoro: variabile contatore che indica se c'è un fattore primo in comune */
     
     /* Confronto i fattori primi di a con quelli di b e c */
-    for(i = 2; i <= a; i++){
-        
+    for(i = 2;
+        i <= a;
+        i++)
+    {
         /* Setto ogni volta la variabile control a 0 
             ogni volta che si ripete il ciclo */
         control = 0;
@@ -177,7 +188,11 @@ void common_first_num(int a,        /* Input: variabile che identifica il valore
         control = validator_first_num(i);
         
         /* Se il fattore è primo verifico che lo sia per tutti */
-        if(control == 1 && a%i == 0 && b%i == 0 && c%i == 0){
+        if(control == 1 &&
+           a%i == 0 && 
+           b%i == 0 && 
+           c%i == 0)
+        {
             
 
             
@@ -185,7 +200,10 @@ void common_first_num(int a,        /* Input: variabile che identifica il valore
             
             
             /* Controllo perchè un numero non può essere fattore primo di se stesso */
-            if(i != a && i != b && i != c){
+            if(i != a &&
+               i != b && 
+               i != c)
+            {
                 printf("Un fattore primo in comune è %d\n",i);
                 count++;
             }
@@ -193,7 +211,8 @@ void common_first_num(int a,        /* Input: variabile che identifica il valore
     }
     
     /* Se non ci sono fattori primi, lo stampo a video */
-    if(count == 0){
+    if(count == 0)
+    {
         printf("Non ci sono fattori primi in comune.\n");
     }  
 }
@@ -208,15 +227,19 @@ void equation_validator(int a,      /* Input: variabile che identifica il valore
 {
     
     /* Calcolo le varie potenze */
-    a = pow(a,x);
-    b = pow(b,y);
-    c = pow(c,z);
+    a = pow(a,
+            x);
+    b = pow(b,
+            y);
+    c = pow(c,
+            z);
     
     /* Sommo a + b */
     a = a + b;
     
     /* Verifico se è valida e se non è così, lo stampo a video */
-    if(a != c){
+    if(a != c)
+    {
         printf("L'equazione non è valida.\n");
     }  
 }
@@ -233,23 +256,36 @@ void beal_conjecture()
     
     /* Acquisisco i dati */
     printf("Inserisci il valore di a: ");
-    a = validator_num(1,'n');
+    a = validator_num(1,
+                      'n');
     printf("Inserisci il valore di b: ");
-    b = validator_num(1,'n');
+    b = validator_num(1,
+                      'n');
     printf("Inserisci il valore di c: ");
-    c = validator_num(1,'n');
+    c = validator_num(1,
+                      'n');
     printf("Inserisci il valore di x: ");
-    x = validator_num(3,'n');
+    x = validator_num(3,
+                      'n');
     printf("Inserisci il valore di y: ");
-    y = validator_num(3,'n');
+    y = validator_num(3,
+                      'n');
     printf("Inserisci il valore di z: ");
-    z = validator_num(3,'n');
+    z = validator_num(3,
+                      'n');
     
     /* Verifico se l'equazione è valida */
-    equation_validator(a,b,c,x,y,z);
+    equation_validator(a,
+                       b,
+                       c,
+                       x,
+                       y,
+                       z);
     
     /* Verifico se sono presenti fattori primi in comune */
-    common_first_num(a,b,c);
+    common_first_num(a,
+                     b,
+                     c);
 
 }
 
@@ -261,24 +297,30 @@ void collatz_conjecture()
     
     printf("Inserisci valore: ");
     /* Acquisisco il valore */
-    val = validator_num(1,'y');
+    val = validator_num(1,
+                        'y');
     
     /* Inizio a stampare la sequenza finchè non arrivo a uno */
     printf("La sequenza è:\n");
     
-    printf("%d\n",val);
-    while(val != 1){
+    printf("%d\n",
+           val);
+    while(val != 1)
+    {
         /* Stabilisco il caso in cui il numero è pari */
-        if(val%2 == 0){
+        if(val%2 == 0)
+        {
             /* Lo divido per due e lo stampo */
             val = val/2;
-            printf("%d\n",val);
-        }else{
+            printf("%d\n",
+                    val);
+        }else
+        {
             /* Se è dispari lo moltiplico per tre, aggiungo uno e lo stampo */
             val = val*3;
             val++;
-            printf("%d\n",val);
-            
+            printf("%d\n",
+                    val); 
         }
     }
 }
@@ -296,7 +338,9 @@ int validator_next_first_num(int val)
     
     /*  Effettuo la verifica su ogni numero successivo al primo
         finchè non trovo quello primo */
-    for(next = val; control == 0; next++)
+    for(next = val;
+        control == 0;
+        next++)
     {
         control = validator_first_num(next);
     }
@@ -309,10 +353,12 @@ int validator_next_first_num(int val)
         quello giusto */
     do{
         /* Acquisisco il valore */
-        scanf("%d",&ins_val);
+        scanf("%d",
+              &ins_val);
 
         /* Se non è il successivo stampo l'errore */
-        if(ins_val != next){
+        if(ins_val != next)
+        {
             fflush(stdin);
             printf("Non è corretto. Riprova: ");
         }
@@ -334,22 +380,26 @@ void cramer_conjecture()
     
     printf("Inserisci un numero primo: ");
     do{
-        val1 = validator_num(12,'y');
+        val1 = validator_num(12,
+                            'y');
         control = validator_first_num(val1);
         
-        if(control == 0){
+        if(control == 0)
+        {
             printf("Non è un numero primo.\n" 
                         "Inserisci di nuovo: ");
         }
-
     }while(control != 1);
     
     val2 = validator_next_first_num(val1);
     abs_diff = abs(val1-val2);
-    printf("Il valore assoluto della differenza tra i due numeri primi è %d\n",abs_diff);
+    printf("Il valore assoluto della differenza tra i due numeri primi è %d\n",
+            abs_diff);
     
     squared_log = log(val1);
-    squared_log = pow(squared_log,2);
-    printf("Il quadrato del logartimo naturale del numero primo più piccolo è %.2f\n",squared_log);
+    squared_log = pow(squared_log,
+                  2);
+    printf("Il quadrato del logartimo naturale del numero primo più piccolo è %.2f\n",
+            squared_log);
 
 }
